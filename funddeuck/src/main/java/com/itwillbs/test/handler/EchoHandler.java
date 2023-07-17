5862c4a037639a70755c94352ac6d07ad2741e73
aimyon <byebyemrmug@naver.com>
2023-07-17 17:25:19 +0900

    first commit

---
+++ funddeuck/src/main/java/com/itwillbs/test/handler/EchoHandler.java
@@ -0,0 +1,5 @@
+package com.itwillbs.test.handler;
+
+public class EchoHandler {
+
+}
---
+++ funddeuck/src/main/java/com/itwillbs/test/mapper/ProjectMapper.java
@@ -0,0 +1,8 @@
+package com.itwillbs.test.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectMapper {
+
+}
