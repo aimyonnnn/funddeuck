1bb7716a51cff406c115098427256deee4e38a45
dust <dust@ITWILL>
2023-07-20 15:12:35 +0900

    refactor : payment.js 리팩토링 feat : xml 아임포트 결제 관련 정보 추가

--- funddeuck/src/main/java/com/itwillbs/test/controller/paymentController.java
+++ funddeuck/src/main/java/com/itwillbs/test/controller/paymentController.java
@@ -1,8 +1,43 @@
 package com.itwillbs.test.controller;
 
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
 
 @Controller
 public class paymentController {
-
-}
+	
+	private IamportClient api;
+	
+	// 아임포트 클라이언트 검증
+	public paymentController() {
+		// REST API 키와 REST API secret
+		this.api = new IamportClient("3162026175407507",
+				"lcNI8uNDYbiIPkE5bUhgjvamgWWvdqmpoCZMrTWvKwslV4uLjsbzjNqnJiibjF3IIFZv3okTRqFNuRYL");
+	}
+	
+	// 결제 정보 검증
+	@ResponseBody
+	@RequestMapping(value = "/verifyIamport/{imp_uid}")
+	public IamportResponse<Payment> paymentByImpUid(
+			Model model,
+			Locale locale,
+			HttpSession session,
+			@PathVariable(value= "imp_uid") String imp_uid) throws IamportResponseException, IOException {
+				return api.paymentByImpUid(imp_uid);
+	} 
+		
+}	
+	
