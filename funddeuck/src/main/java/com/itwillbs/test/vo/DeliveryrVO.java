3611b7e629b69d726990ae505ca030d23df3a9cc
bohee <bohee425@gmail.com>
2023-07-21 12:30:23 +0900

    fix DeliveryVO, PaymentVO 수정

---
+++ funddeuck/src/main/java/com/itwillbs/test/vo/DeliveryrVO.java
@@ -0,0 +1,34 @@
+package com.itwillbs.test.vo;
+
+import lombok.Data;
+
+/*
+배송 정보를 관리하는 delivery 테이블 정의
+---------------------------------------
+회원정보 (member_idx) - 정수, FK
+리워드 배송 정보 저장 상태 판별(delivery_adr_status) - 정수
+리워드 배송지 주소 (delivery_address) - 문자(500자)
+리워드 수령자 전화번호(delivery_phone_number) - 문자(10자)
+---------------------------------------
+CREATE TABLE delivery (
+	delivery_idx INT PRIMARY KEY AUTO_INCREMENT COMMENT '배송지번호(PK)',
+	member_idx INT NOT NULL COMMENT '회원번호(FK)',
+	delivery_reciever VARCHAR(50) NOT NULL COMMENT '수취인',
+	delivery_phone VARCHAR(50) NOT NULL COMMENT '수취인연락처',
+	delivery_zipcode VARCHAR(10) NOT NULL COMMENT '수취인우편번호',
+  	delivery_add VARCHAR(500) NOT NULL COMMENT '수취인주소',
+  	delivery_detailadd VARCHAR(500) NOT NULL COMMENT '수취인상세주소',
+ 	FOREIGN KEY (member_idx) REFERENCES members(member_idx)    
+);
+*/
+@Data
+public class DeliveryrVO {
+	private int delivery_idx;
+	private int member_idx;
+	private String delivery_reciever;
+	private String delivery_phone;
+	private String delivery_zipcode;
+	private String delivery_add;
+	private String delivery_detailadd;
+	
+}
--- funddeuck/src/main/java/com/itwillbs/test/vo/PaymentVO.java
+++ funddeuck/src/main/java/com/itwillbs/test/vo/PaymentVO.java
@@ -20,29 +20,51 @@ import lombok.Data;
 리워드 수령자 전화번호(delivery_phone_number) - 문자(10자)
 ---------------------------------------
 CREATE TABLE payment (
-	payment_idx INT PRIMARY KEY AUTO_INCREMENT,
-	payment_date DATE NOT NULL,
-	reward_name VARCHAR(50) NOT NULL,
-	reward_amount INT NOT NULL,
-	use_coupon_amount INT,
-	donation INT,
-	total_amount INT NOT NULL,
-	delivery_address VARCHAR(500)
-	delivery_phone_number VARCHAR(10)
-	FOREIGN KEY (member_idx) REFERENCES member(member_idx)
-	FOREIGN KEY (project_idx) REFERENCES project(project_idx)
+	payment_idx INT PRIMARY KEY AUTO_INCREMENT COMMENT '주문번호(PK)',
+	member_idx INT NOT NULL COMMENT '회원번호(FK)',
+	project_idx INT NOT NULL COMMENT '프로젝트번호(FK)',
+	reward_idx INT NOT NULL COMMENT '리워드번호(FK)',	
+	delivery_idx INT NOT NULL  COMMENT '배송지번호(FK)',
+	member_email VARCHAR(50) NOT NULL COMMENT '회원이메일',
+	member_phone VARCHAR(50) NOT NULL COMMENT '회원연락처',	
+	reward_amount INT NOT NULL COMMENT '리워드금액',
+	additional_amount INT COMMENT '추가후원금',
+	use_coupon_amount INT  COMMENT '쿠폰금액',
+	total_amount INT NOT NULL COMMENT '최종결제금액',
+	payment_date DATE NOT NULL COMMENT '주문날짜',
+	payment_quantity INT NOT NULL COMMENT '주문수량',
+	payment_confirm INT NOT NULL DEFAULT 1 COMMENT '결제승인여부(1:결제완료,2:취소요청,3:취소완료)', 
+	refund_bank VARCHAR(50) COMMENT '환급받을은행명',
+	refund_accountnum VARCHAR(50) COMMENT '환급받을계좌번호',
+	courier VARCHAR(50) COMMENT '택배사',
+	waybill_num VARCHAR(50) COMMENT '운송장번호',
+	delivery_status INT COMMENT '배송상황(1:미정,2:배송준비중,3:배송시작,4:배송완료,5:프로젝트취소',
+	FOREIGN KEY (member_idx) REFERENCES members(member_idx),
+	FOREIGN KEY (project_idx) REFERENCES project(project_idx),
+	FOREIGN KEY (reward_idx) REFERENCES reward(reward_idx),
+	FOREIGN KEY (delivery_idx) REFERENCES delivery(delivery_idx)
 );
 */
 @Data
 public class PaymentVO {
 	private int payment_idx;
-	private Date payment_date;
-	private String reward_name;
-	private int reward_amount;
-	private int donation;
-	private int total_amount;
-	private String delivery_address;
-	private String delivery_phone_number;
 	private int member_idx;
 	private int project_idx;
+	private int reward_idx;
+	private int delivery_idx;
+	private String member_email;
+	private String member_phone;
+	private int reward_amount;
+	private int additional_amount;
+	private int use_coupon_amount;
+	private int total_amount;
+	private Date payment_date;
+	private int payment_quantity;
+	private int payment_confirm;
+	private String refund_bank;
+	private String refund_accountnum;
+	private String courier;
+	private String waybill_num;
+	private int delivery_status;
+	
 }
